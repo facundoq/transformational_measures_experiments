519b66bfcf75a98283d2af0e9ba75e57d8ec528b
Facundo Quiroga <facundoq@gmail.com>
2019-11-12 12:47:47 -0300

    implemented goodfellow's measure

--- transformation_measure/measure/goodfellow.py
+++ transformation_measure/measure/goodfellow.py
@@ -1,53 +1,125 @@
-from .base import Measure,MeasureResult
-import numpy as np
-from .layer_transformation import ConvAggregation
-from transformation_measure.measure.stats_running import RunningMeanAndVariance,RunningMean
+from transformation_measure import Measure,ActivationsIterator,MeasureResult
 import transformation_measure as tm
 from multiprocessing import Queue
-from transformation_measure import MeasureFunction
-
 from .multithreaded_layer_measure import LayerMeasure,PerLayerMeasure,ActivationsOrder
+import numpy as np
+from transformation_measure.measure.stats_running import RunningMeanAndVariance,RunningMean,RunningMeanSimple
 
 class GlobalVariance(LayerMeasure):
 
+    def __init__(self, id:int, name:str, activation_percentage:float,sign:int):
+        super().__init__(id,name)
+        self.activation_percentage = activation_percentage
+        self.sign=sign
+
     def eval(self,q:Queue,inner_q:Queue):
-        inner_m = RunningMeanAndVariance()
-        self.m = RunningMean()
-        for iteration_info in self.queue_as_generator(q):
+        max_samples_per_transformation=100
+        history=[]
+
+        for transformation in self.queue_as_generator(q):
+            i=0
             for activations in self.queue_as_generator(inner_q):
-                activations = self.preprocess_activations(activations)
-                for j in range(activations.shape[0]):
-                    inner_m.update(activations[j,])
-            inner_result=self.measure_function.apply_running(inner_m)
-            self.m.update(inner_result)
+                if i<max_samples_per_transformation:
+                    history.append(activations)
+                i+=1
+
+        # sort all activation values for the different samples
+        activations = np.vstack(history)
+        if self.sign != 1:
+            activations*=self.sign
+        activations.sort(axis=0)
+        n = activations.shape[0]
+        # calculate the threshold indexes so that G(i) = activation_percentage
+        threshold_indexes = round((1-self.activation_percentage)*n)
+        # calculate the threshold values (approximately)
+        self.thresholds = activations[threshold_indexes, :]
+
+        # set g(i) equal to the activations_percentage
+        self.g= np.zeros_like(self.thresholds) + self.activation_percentage
+
 
     def get_final_result(self):
-        return self.m.mean()
+        return self.g
+
+class LocalVariance(LayerMeasure):
+
+    def __init__(self, id:int, name:str, threshold:float,sign:int):
+        super().__init__(id,name)
+        self.threshold = threshold
+        self.sign=sign
+
+    def eval(self,q:Queue,inner_q:Queue):
+        running_mean = RunningMeanSimple()
+        activation_sum=0
+        n=0
+        for transformation in self.queue_as_generator(q):
+            for activations in self.queue_as_generator(inner_q):
+                if self.sign != 1:
+                    activations *= self.sign
+                activated = (activations > self.threshold) * 1
+                running_mean.update_all(activated)
+
+        self.l=running_mean.mean()
+
+
+
+
+
+    def get_final_result(self):
+        return self.l
 
 
 class GlobalFiringRateMeasure(PerLayerMeasure):
-    def __init__(self):
+    def __init__(self,activation_percentage:float,sign:int):
         super().__init__(ActivationsOrder.TransformationsFirst)
+        self.activation_percentage:float=activation_percentage
+        self.sign:int=sign
+        self.layer_measures:{int,GlobalVariance}={}
+
     def __repr__(self):
         return f"G()"
 
     def generate_layer_measure(self, i:int, name:str) -> LayerMeasure:
-        return GlobalVariance(i, name)
+        layer_measure = GlobalVariance(i, name,self.activation_percentage,self.sign)
+        self.layer_measures[i]=layer_measure
+        return layer_measure
+
+    def get_layer_measure(self,i:int):
+        return self.layer_measures[i]
+
+    def get_thresholds(self):
+        return {i:m.thresholds  for i,m in self.layer_measures.values()}
 
 class LocalFiringRateMeasure(PerLayerMeasure):
-    def __init__(self):
+    def __init__(self,thresholds:[np.ndarray],sign:int):
         super().__init__(ActivationsOrder.SamplesFirst)
+        self.sign=sign
+        self.thresholds=thresholds
+
     def __repr__(self):
         return f"L()"
 
     def generate_layer_measure(self, i:int, name:str) -> LayerMeasure:
-        return GlobalVariance(i, name)
 
-class Goodfellow():
+        return GlobalVariance(i, name,self.thresholds[i],self.sign)
 
-    def __init__(self,):
-        sm = GlobalFiringRateMeasure()
-        ttm = LocalFiringRateMeasure()
+class Goodfellow(Measure):
+
+    def __init__(self,activations_percentage=0.01,sign=1):
+        assert sign in [1,-1]
+        self.activations_percentage=activations_percentage
+        self.sign=sign
+
+    def eval(self,activations_iterator:ActivationsIterator):
+        self.g = GlobalFiringRateMeasure(self.activations_percentage,self.sign)
+        g_result = self.g.eval(activations_iterator)
+
+        self.thresholds = self.g.get_thresholds()
+        self.l = LocalFiringRateMeasure(self.thresholds,self.sign)
+        l_result = self.l.eval(activations_iterator)
+
+        ratio = tm.divide_activations(l_result.layers,g_result.layers)
+        return MeasureResult(ratio,activations_iterator.activation_names(),self)
 
     def __repr__(self):
-        return f"Goodfellow()"
+        return f"Goodfellow(gp={self.activations_percentage})"
--- transformation_measure/measure/stats_running.py
+++ transformation_measure/measure/stats_running.py
@@ -24,29 +24,59 @@ class RunningMeanSingle:
     def mean(self):
         return self.new_m if self.n else 0.0
 
+class RunningMeanSimple:
+    def __repr__(self):
+        return f"RunningMeanSimple(n={self.n},mean={self.sum().shape})"
+
+    def __init__(self):
+        self.clear()
+
+    def clear(self):
+        self.n = 0
+        self.sum = 0
+
+    def update(self, x):
+        self.n += 1
+        self.sum+=x
+
+    def update_all(self,x:np.ndarray):
+        self.sum += x.sum(axis=0)
+        self.n += x.shape[0]
+
+    def mean(self):
+        return self.sum if self.n > 0 else 0.0
+
 class RunningMean:
     def __repr__(self):
         return f"RunningMean(n={self.n},mean={self.mean().shape})"
 
     def __init__(self):
-        self.n = 0
-        self.old_m = 0
-        self.new_m = 0
+        self.clear()
 
     def clear(self):
         self.n = 0
+        self.mean = 0
 
     def update(self, x):
         self.n += 1
 
         if self.n == 1:
-            self.old_m = self.new_m = x
+            self.mean = x
         else:
-            self.new_m = self.old_m + (x - self.old_m) / self.n
-            self.old_m = self.new_m
+            self.mean = self.mean + (x - self.mean) / self.n
+
+    def update_all(self,x:np.ndarray):
+        k=x.shape[0]
+        if self.n == 0:
+            self.mean = x.mean(axis=0)
+            self.n = k
+        else:
+            self.n += k
+            x_sum=x.sum(axis=0)
+            self.mean = self.mean + (x_sum - self.mean * k) / self.n
 
     def mean(self):
-        return self.new_m if self.n else 0.0
+        return self.mean if self.n>0 else 0.0
 
 
 class RunningMeanAndVariance:
